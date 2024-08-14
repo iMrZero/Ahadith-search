0b6316e318b29fd9652afd2cf888973fcf63d99a
imrzero <abdullatifrabie@gmail.com>
2024-08-15 00:28:46 +0300

    create new files

---
+++ script.js
@@ -0,0 +1,18 @@
+const api_url = "https://dorar.net/dorar_api.json?skey=";
+const searchBtn = document.getElementById("search");
+async function fetchData(param) {
+  try {
+    const respone = await fetch(param);
+    const data = await respone.json();
+    return data;
+  } catch (error) {
+    console.log(`unable to fetch date from ${url} : ${error}`);
+  }
+}
+searchBtn.addEventListener("click", async () => {
+  const searchKey = document.getElementById("key-word").value;
+  const resualt = document.getElementById("result");
+  const data = await fetchData(api_url + searchKey);
+  resualt.innerHTML = data.ahadith.result.replace(/<br\/>|-*/g, "");
+  console.log(data);
+});
